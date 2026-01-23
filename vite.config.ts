dccaa200733e856aa0722b687ac78e701cb3af6e
Peter Gaard <pgaard@gmail.com>
2026-01-22 20:32:57 -0600

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: '/',
-    plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+    return {
+        base: mode === 'production' ? '/wordle/' : '/',
+        plugins: [react()],
+    }
 })
